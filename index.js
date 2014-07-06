5a494490a49b7842b79984f60ae02819f4b681d4
Eric Gjertsen <ericgj72@gmail.com>
2014-07-05 20:45:32 -0400

    add tests: casts

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var extend = require('extend')
   , dispatch = require('d3-dispatch')
   , rebind   = require('d3-rebind')
+  , has      = hasOwnProperty
 
 // Note: JSON Schema types
 
@@ -28,8 +29,8 @@ module.exports = function(){
   
   model.attr = function(_,schema){
     schema = schema || {}
-    if (schema.default) defaults[_] = defaultfn(schema.default);
-    if (schema.type)    casts[_]    = castfn(schema.type, castfns);
+    if (schema.default)  defaults[_] = defaultfn(schema.default);
+    if (schema.type)     casts[_]    = castfn(schema.type, castfns);
     return this;
   }
 
@@ -37,7 +38,7 @@ module.exports = function(){
 
     var changes = []
       , self = {}
-      , dispatcher = dispatch('set')
+      , dispatcher = dispatch('set','reset')
 
     self.get = function(attr){
       return this.value()[attr];
@@ -49,18 +50,24 @@ module.exports = function(){
         return this;
       }
       changes.push(attr,val);
-      dispatcher.set(attr, val)
+      dispatcher.set(attr, val);
       return this;
     }
 
     self.value = function(){
       var raw = changeObj( applyObj(obj, defaults), changes);
-      return applyObj( raw, casts );
+      return applyUpdateObj( raw, casts );
+    }
+
+    self.changes = function(){
+      var raw = changeObj( {}, changes );
+      return applyUpdateObj( raw, casts );
     }
 
     self.reset = function(_){
       changes = [];
       obj = Object(_);
+      dispatcher.reset();
       return this;
     }
 
@@ -83,14 +90,16 @@ function defaultfn(defval){
 
 function castfn(type, fns){
   fns = fns || {};
+  type = type || function(v){ return v; };
   return function(val){
     if (undefined === val) return val;
     if ('function' === typeof type) return type(val);
-    if (!fns[type]) throw new TypeError("Unable to cast to unknown type: " + type);
+    if (!has.call(fns,type)) throw new TypeError("Unable to cast to unknown type: " + type);
     return fns[type](val);
   }
 }
 
+// add + update values
 function changeObj(obj, chgs){
   var ret = extend({},obj);
   for (var i=0, chg=chgs[i]; i<chgs.length; ++i){
@@ -99,10 +108,20 @@ function changeObj(obj, chgs){
   return ret;
 }
 
+// add + update values
 function applyObj(obj, attrfn){
   var ret = extend({},obj);
   for (var k in attrfn){
     ret[k] = attrfn[k](obj[k]);
+  }
+  return ret;
+}
+
+// update values
+function applyUpdateObj(obj, attrfn){
+  var ret = extend({},obj);
+  for (var k in obj){
+    if (has.call(attrfn,k)) ret[k] = attrfn[k](obj[k]);
   }
   return ret;
 }
--- test/tests.js
+++ test/tests.js
@@ -3,7 +3,7 @@ var model = require('model')
   , assert = require('assert')
   , has = hasOwnProperty
 
-describe('model default', function(){
+describe('defaults', function(){
 
   var subject = 
     model().attr('A', { default: 'default' } )
@@ -12,7 +12,7 @@ describe('model default', function(){
 
   it('sets defaults on empty object', function(){
     var actual = subject().value();
-    console.log('defaults on empty object: %o', actual);
+    console.log('model: defaults: empty object: %o', actual);
     assert.equal( actual.A, 'default' );
     assert.deepEqual( actual.B, [] );
   })
@@ -24,17 +24,71 @@ describe('model default', function(){
 
   it('sets defaults on non-empty object for attributes not set', function(){
     var actual = subject({ A: "nondefault" }).value();
-    console.log('defaults on non-empty object: %o', actual);
+    console.log('model: defaults: non-empty object: %o', actual);
     assert.equal( actual.A, "nondefault" );
     assert.deepEqual( actual.B, []);
   })
 
-  it('passes through non-empty object attributes that have not been defined', function(){
+  it('passes through attributes that have not been defined', function(){
     var actual = subject({ B: [1,2], D: "something new"}).value();
-    console.log('undefined attributes on non-empty object: %o', actual);
+    console.log('model: defaults: undefined attributes: %o', actual);
     assert.equal( actual.A, "default" );
     assert.deepEqual( actual.B, [1,2]);
     assert.equal( actual.D, "something new" );
   })
 
 })
+
+describe('casts', function(){
+
+  var subject =
+    model().attr('string',  { type: 'string' })
+           .attr('boolean', { type: 'boolean' })
+           .attr('number',  { type: 'number' })
+           .attr('integer', { type: 'integer' })
+           .attr('null',    { type: 'null' })
+           .attr('array',   { type: 'array' })
+           .attr('object',  { type: 'object' })
+           .attr('custom',  { type: 'upcase' })
+           .attr('notype')
+           .cast('upcase', function(v){ return String(v).toUpperCase(); })
+
+  it('casts not applied to empty object', function(){
+    var actual = subject().value();
+    console.log('model: casts: empty object: %o', actual);
+    assert.deepEqual( actual, {});
+  })
+  
+  it('passes through attributes that have not been defined', function(){
+    var actual = subject({ 'boolean': 'yes', 'foo': '42' }).value();
+    console.log('model: casts: undefined attributes: %o', actual);
+    assert.equal( actual.foo, '42' );
+  })
+
+  it('casts applied to string values', function(){
+    var actual = subject({
+      'string': '1', 
+      'boolean': '2', 
+      'number': '3',
+      'integer': '4',
+      'null': '5',
+      'array': '6',
+      'object': '7',
+      'custom': '8',
+      'notype': '9'
+    }).value()
+
+    console.log('model: casts: applied to string values: %o', actual);
+    assert.equal( typeof actual['string'], 'string' );
+    assert.equal( typeof actual['boolean'], 'boolean' );
+    assert.equal( typeof actual['number'], 'number' );
+    assert.equal( typeof actual['integer'], 'number' );
+    assert.equal( actual['null'], null );
+    assert( actual['array'] instanceof Array );
+    assert.equal( typeof actual['object'], 'object' );
+    assert.equal( actual['custom'], '8' );
+    assert.equal( actual['notype'], '9' );
+  })
+
+})
+
